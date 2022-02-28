ae78d492270a3f777b80cc2cce8b8a9a0eba9b04
aw976 <sunmoon1030@gmail.com>
2022-02-28 15:04:25 +0900

    v2

--- getRealTimeBusLocation.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList'
-params ={'serviceKey' : 'VjUEXiL92IwsBXZqkE86F5mTFht4+ut/rxwUxQNWyc5AGu/f5M5LTOZkvxfBfQzWTaRF9fItvRe96V6ZVXgJEQ==', 'routeId' : '234000316' }
-
-response = requests.get(url, params=params)
-print(response.text)
